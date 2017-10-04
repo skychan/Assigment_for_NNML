1947250e1c31eee5acfd4ea8f9199fe32e5a05b9
sky <diufanshu@gmail.com>
2017-10-04 15:25:21 +0800

    before merge

--- Assignment3/a3.m
+++ Assignment3/a3.m
@@ -140,12 +140,27 @@ function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>
   % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
   % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
-
+  
+  % 1. Feed-forward to compute the outvalue
+  hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
+  hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
+  class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
+  class_normalizer = log_sum_exp_over_rows(class_input); % log(sum(exp of class_input)) is what we subtract to get properly normalized log class probabilities. size: <1> by <number of data cases>
+  log_class_prob = class_input - repmat(class_normalizer, [size(class_input, 1), 1]); % log of probability of each class. size: <number of classes, i.e. 10> by <number of data cases>
+  class_prob = exp(log_class_prob); % probability of each class. Each column (i.e. each case) sums to 1. size: <number of classes, i.e. 10> by <number of data cases>
+  % 2. Then, calculate the difference between target
+  delta_ho = class_prob - data.targets;  % <10 x cases>
+  % 3. Finally, back-propgate the difference
+  delta_ih = model.hid_to_class' * delta_ho  .* hid_output .* (1 - hid_output) ; % <hid, cases>
+  
+  J = size(data.inputs, 2);
+  
+  
   % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
-	 
+  
   % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to replace that by a correct computation.
-  ret.input_to_hid = model.input_to_hid * 0;
-  ret.hid_to_class = model.hid_to_class * 0;
+  ret.input_to_hid = (1/J) .* (delta_ih * data.inputs') + wd_coefficient .* model.input_to_hid;
+  ret.hid_to_class = (1/J) .* (delta_ho * hid_output') + wd_coefficient .* model.hid_to_class;
 end
 
 function ret = model_to_theta(model)
